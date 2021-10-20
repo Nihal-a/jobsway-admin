1e2344c90dea1b49da101d3de3d0a91d0c84640e
Nihal-a <nihalavulan1@gamil.com>
2021-10-21 00:29:36 +0530

    register page created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 
 function App() {
   return (
     <div className="App">
-            <Dashboard/>
+            {/* <Dashboard/> */}
       {/* <Login/> */}
+      <Register /> 
     </div>
   );
 }
